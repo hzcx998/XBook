ff1c66fcc97a900f4ef4ba48b6318dcb82681d65
Hu Zicheng <2323168280@qq.com>
2019-06-02 01:07:39 +0800

    update

---
+++ src/init/main.c
@@ -0,0 +1,16 @@
+/*
+ File:		init/main.c
+ Auther:	Jason Hu
+ Time:		2019/6/2
+ Copyright:	(C) 2018-2019 by BookOS developers. All rights reserved.
+ */
+
+int main()
+{
+	
+	while (1) {
+		
+	}
+	
+	return 0;
+}
